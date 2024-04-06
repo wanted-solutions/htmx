2e1b1107b010e5a6207a827554a049bd966e85f7
Martin Hrášek <martin.hrasek@drmax.eu>
2024-04-06 22:55:53 +0200

    Add basic server

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,8 +1,45 @@
 package server
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
 
-func Start() {
-	// Start the server
-	fmt.Println("Starting the server...")
+	"github.com/gin-gonic/gin"
+)
+
+func New() *Engine {
+	return &Engine{
+		handler: gin.New(),
+	}
+}
+
+type Engine struct {
+	handler *gin.Engine
+	server  *http.Server
+}
+
+func (e *Engine) Use(middleware ...gin.HandlerFunc) {
+	e.handler.Use(middleware...)
+}
+
+func (e *Engine) LoadTemplates(pattern string) {
+	e.handler.LoadHTMLGlob(pattern)
+}
+
+// Public functions
+func (e *Engine) ListenAndServe(addr string) error {
+	e.createHTTPServer(addr)
+	return e.server.ListenAndServe()
+}
+
+// Private functions
+func (e *Engine) createHTTPServer(addr string) {
+	e.server = &http.Server{
+		Addr:           fmt.Sprintf(":%s", addr),
+		Handler:        e.handler,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
 }
